c48a3aa437b3a9ce276e5529d38c73e945830df0
Tomas Tulka <tomas.tulka@gmail.com>
2018-09-21 12:26:49 +0200

    MethodMetadata interface used

--- beans-ordering/default-impl/src/main/java/cz/net21/ttulka/test/OnMissingBeanCondition.java
+++ beans-ordering/default-impl/src/main/java/cz/net21/ttulka/test/OnMissingBeanCondition.java
@@ -3,12 +3,12 @@ package cz.net21.ttulka.test;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
-import org.springframework.core.type.classreading.MethodMetadataReadingVisitor;
+import org.springframework.core.type.MethodMetadata;
 
 public class OnMissingBeanCondition implements Condition {
 
     @Override
     public boolean matches(ConditionContext conditionContext, AnnotatedTypeMetadata annotatedTypeMetadata) {
-        return !conditionContext.getRegistry().containsBeanDefinition(((MethodMetadataReadingVisitor) annotatedTypeMetadata).getMethodName());
+        return !conditionContext.getRegistry().containsBeanDefinition(((MethodMetadata) annotatedTypeMetadata).getMethodName());
     }
 }
