a4f5772a6b097e20d368ed69b664ac26caf1d33a
Tomas Tulka <tomas.tulka@gmail.com>
2020-09-07 09:02:54 +0200

    reactive demo init

---
+++ reactive-demo/src/main/java/com/ttulka/samples/reactive/ReactiveDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ttulka.samples.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveDemoApplication.class, args);
+	}
+
+}
---
+++ reactive-demo/src/test/java/com/ttulka/samples/reactive/ReactiveDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttulka.samples.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
