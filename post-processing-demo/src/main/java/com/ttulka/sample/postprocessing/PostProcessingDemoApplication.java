c36dbafd9ba3763d46fe29b9df1696a15facad37
Tomas Tulka <tomas.tulka@gmail.com>
2020-10-21 09:05:12 +0200

    post processors demo

---
+++ post-processing-demo/src/main/java/com/ttulka/sample/postprocessing/PostProcessingDemoApplication.java
@@ -0,0 +1,50 @@
+package com.ttulka.sample.postprocessing;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class PostProcessingDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostProcessingDemoApplication.class, args);
+    }
+
+    @Component
+    static class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+        @Override
+        public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+            System.out.println(this.getClass().getSimpleName());
+        }
+    }
+
+    @Component
+    static class MyBeanPostProcessor implements BeanPostProcessor {
+        @Override
+        public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+            System.out.println(this.getClass().getSimpleName() + " postProcessBeforeInitialization");
+            return bean;
+        }
+
+        @Override
+        public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+			System.out.println(this.getClass().getSimpleName() + " postProcessAfterInitialization");
+        	return bean;
+        }
+    }
+
+	@Component
+	static class MyEnvironmentPostProcessor implements EnvironmentPostProcessor {
+		@Override
+		public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+			System.out.println(this.getClass().getSimpleName());
+		}
+	}
+}
---
+++ post-processing-demo/src/test/java/com/ttulka/sample/postprocessing/PostProcessingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttulka.sample.postprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostProcessingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
