80f7427f6664059d7935f840e653a6f12a7600f5
Tomas Tulka <tomas.tulka@gmail.com>
2021-01-19 18:45:14 +0100

    more things

--- transactional-messaging/src/main/java/com/ttulka/samples/messaging/biz/MyService.java
+++ transactional-messaging/src/main/java/com/ttulka/samples/messaging/biz/MyService.java
@@ -4,6 +4,7 @@ import com.ttulka.samples.messaging.jpa.MyEntity;
 import com.ttulka.samples.messaging.jpa.MyRepository;
 import com.ttulka.samples.messaging.amqp.Sender;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,15 +21,19 @@ public class MyService {
 
         sender.sendMessage("Hello!");
 
-        if (true) throw new RuntimeException("Oops!");
+        if (true) throw new RuntimeException("Oops! 1");
 
         myRepository.save(new MyEntity(null, "Test 2"));
     }
 
     @Transactional
+    @SneakyThrows
     public void saveSomething() {
         myRepository.save(new MyEntity(null, "Test A"));
+        System.out.println("Sending 'saved' message.");
         sender.sendMessage("Saved!");
+        Thread.sleep(1000);
+        System.out.println("'saved' message sent.");
         myRepository.save(new MyEntity(null, "Test B"));
     }
 
@@ -39,7 +44,7 @@ public class MyService {
 
             sender.sendMessage("Updated! " + myEntity.getId());
 
-            if (true) throw new RuntimeException("Oops!");
+            if (true) throw new RuntimeException("Oops! 2");
 
             //myRepository.save(myEntity); // not necessary in @Transactional
         }
